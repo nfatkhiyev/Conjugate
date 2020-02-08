c8d1ff62a5583f9c9e5dc194f6672965ecb7c346
Nate Fatkhiyev <nfatkhiyev@gmail.com>
2020-02-08 17:23:07 -0500

    add return and delete hw

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 import os
+import json
 from flask import request, jsonify
 from app import app
 import requests
@@ -27,7 +28,7 @@ def add_homework():
     homework_due_date = body['homework_due_date']
 
     matched_class = Classes.query.filter(Classes.class_name == class_name).filter(Classes.class_start_time == class_start_time).filter(Classes.class_end_time == class_end_time).filter(Classes.class_building == class_building).filter(Classes.class_room_number).all()
-    if matched_class = None:
+    if matched_class == None:
         new_class = Classes(class_name, class_start_time, class_end_time, class_building, class_room_number)
         db.session.add(new_class)
         db.session.flush()
@@ -44,3 +45,39 @@ def add_homework():
     db.session.expire(homeowrk)
 
     return "homework added to db"
+
+@app.route("/remove_homework")
+def remove_homework():
+    body = request.json
+    homework_id = body['homework_id']
+    user_name = body['user_name']
+
+    homework = Homeworks.query.filter(Homeworks.user_name == user_name).filter(Homeworks.homework_id == homework_id).first()
+
+    if homework == None:
+        return "User not authorized to remove this homework"
+
+    db.session.delete(homework)
+    db.session.flush()
+    db.session.commit()
+
+    return "Homework has been deleted"
+
+@app_route("get_homework/<string:user_name>", methods=["POST"])
+def get_homework(user_name):
+    homeworks = Homeworks.query.filter_by(user_name=user_name).all()
+
+    json = { "user_name":str(user_name) }
+
+    count = 0
+    for homework in homeworks:
+        class_info = Classes.query.filter_by(class_id=homework.class_id).first()
+        hw_json = { "homework_"+str(count): [{
+                    "Title":str(homework.homework_title),
+                    "Class":str(class_info.class_name),
+                    "Due":str(homework.homework_due_date),
+                    }]}
+        json.update(hw_json)
+        count+=count
+
+    return json
