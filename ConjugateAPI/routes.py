0e853228a796da17bb264f0e1d15fb91bcee8905
Nate Fatkhiyev <nfatkhiyev@gmail.com>
2020-03-09 10:24:31 -0400

    use login decorators

--- ConjugateAPI/routes.py
+++ ConjugateAPI/routes.py
@@ -6,7 +6,7 @@ from ConjugateAPI import app
 import requests
 import flask_migrate
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 db = SQLAlchemy(app)
 migrate = flask_migrate.Migrate(app, db)
@@ -18,74 +18,71 @@ requests.packages.urllib3.disable_warnings()
 
 
 @app.route("/homework", methods=["POST"])
+@login_required
 def add_homework():
-    if is_user_authenticated():
-        body = request.json
-        user_email = body["user_email"]
-        class_name = body["class_name"]
-        class_start_time = body["class_start_time"]
-        class_end_time = body["class_end_time"]
-        class_building = body["class_building"]
-        class_room_number = body["class_room_number"]
-        homework_title = body["homework_title"]
-        homework_due_date = body["homework_due_date"]
+    body = request.json
+    user_email = body["user_email"]
+    class_name = body["class_name"]
+    class_start_time = body["class_start_time"]
+    class_end_time = body["class_end_time"]
+    class_building = body["class_building"]
+    class_room_number = body["class_room_number"]
+    homework_title = body["homework_title"]
+    homework_due_date = body["homework_due_date"]
 
-        if current_user.email == user_email:
+    if current_user.email == user_email:
 
-            # pass without slashes. ex. yyyymmdd
-            current_date = date.today()
-            # due_date = homework_due_date
-            # due_date = datetime(year=int(due_date[0:4]), month=int(due_date[4:6]), day=int(due_date[6:8]))
-            matched_class = (
-                Classes.query.filter(Classes.class_name == class_name)
-                .filter(Classes.class_start_time == class_start_time)
-                .filter(Classes.class_end_time == class_end_time)
-                .filter(Classes.class_building == class_building)
-                .filter(Classes.class_room_number == class_room_number)
-                .first()
+        # pass without slashes. ex. yyyymmdd
+        current_date = date.today()
+        # due_date = homework_due_date
+        # due_date = datetime(year=int(due_date[0:4]), month=int(due_date[4:6]), day=int(due_date[6:8]))
+        matched_class = (
+            Classes.query.filter(Classes.class_name == class_name)
+            .filter(Classes.class_start_time == class_start_time)
+            .filter(Classes.class_end_time == class_end_time)
+            .filter(Classes.class_building == class_building)
+            .filter(Classes.class_room_number == class_room_number)
+            .first()
+        )
+        print(matched_class)
+
+        if matched_class == None or matched_class == []:
+            new_class = Classes(
+                class_name,
+                class_start_time,
+                class_end_time,
+                class_building,
+                class_room_number,
             )
-            print(matched_class)
-
-            if matched_class == None or matched_class == []:
-                new_class = Classes(
-                    class_name,
-                    class_start_time,
-                    class_end_time,
-                    class_building,
-                    class_room_number,
-                )
-                db.session.add(new_class)
-                db.session.flush()
-                db.session.commit()
-                db.session.expire(new_class)
-                matched_class = Classes.query.order_by(Classes.class_id.desc()).first()
-
-            class_id = matched_class.class_id
-            homework = Homeworks(
-                user_email, class_id, homework_title, homework_due_date, current_date
-            )
-
-            db.session.add(homework)
+            db.session.add(new_class)
             db.session.flush()
             db.session.commit()
-            db.session.expire(homework)
+            db.session.expire(new_class)
+            matched_class = Classes.query.order_by(Classes.class_id.desc()).first()
 
-            homework_id = (
-                Homeworks.query.order_by(Homeworks.homeworks_id.desc())
-                .first()
-                .homeworks_id
-            )
+        class_id = matched_class.class_id
+        homework = Homeworks(
+            user_email, class_id, homework_title, homework_due_date, current_date
+        )
 
-            return_json = {"homeworks_id": str(homework_id)}
+        db.session.add(homework)
+        db.session.flush()
+        db.session.commit()
+        db.session.expire(homework)
 
-            return return_json, 200
+        homework_id = (
+            Homeworks.query.order_by(Homeworks.homeworks_id.desc()).first().homeworks_id
+        )
 
-        return "you are not the correct user", 403
+        return_json = {"homeworks_id": str(homework_id)}
 
-    return "User not logged in", 403
+        return return_json, 200
+
+    return "you are not the correct user", 403
 
 
 @app.route("/homework", methods=["DELETE"])
+@login_required
 def remove_homework():
     body = request.json
     homeworks_id = body["homework_id"]
@@ -108,34 +105,37 @@ def remove_homework():
 
 
 @app.route("/homework/<string:user_email>")
+@login_required
 def get_homework(user_email):
-    if current_user.is_authenticated:
-        homeworks = Homeworks.query.filter_by(user_email=user_email).all()
+    if user_email != current_user.email:
+        return "incorrect user"
 
-        json = {"user_name": str(current_user.user_name)}
+    homeworks = Homeworks.query.filter_by(user_email=user_email).all()
 
-        count = 0
-        for homework in homeworks:
-            class_info = Classes.query.filter_by(class_id=homework.class_id).first()
-            hw_json = {
-                "homework_"
-                + str(count): [
-                    {
-                        "homeworks_id": str(homework.homeworks_id),
-                        "Title": str(homework.homework_title),
-                        "Class": str(class_info.class_name),
-                        "Due": homework.homework_due_date.strftime("%d/%m/%y"),
-                    }
-                ]
-            }
-            json.update(hw_json)
-            count += count
+    json = {"user_name": str(current_user.user_name)}
 
-        return json, 200
-    return "Not logged in", 403
+    count = 0
+    for homework in homeworks:
+        class_info = Classes.query.filter_by(class_id=homework.class_id).first()
+        hw_json = {
+            "homework_"
+            + str(count): [
+                {
+                    "homeworks_id": str(homework.homeworks_id),
+                    "Title": str(homework.homework_title),
+                    "Class": str(class_info.class_name),
+                    "Due": homework.homework_due_date.strftime("%d/%m/%y"),
+                }
+            ]
+        }
+        json.update(hw_json)
+        count += count
+
+    return json, 200
 
 
 @app.route("/check_homework/<int:date>")
+@login_required
 def check_homework(date):
     body = request.json
     class_name = body["class_name"]
